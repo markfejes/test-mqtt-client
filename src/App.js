3a4a39b752c92749ecc416fe8bb49d30995e808c
Mark Fejes <fejes.mark92@gmail.com>
2017-10-18 12:22:37 +0200

    basic mqtt connection

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+// material-ui
 import Paper from 'material-ui/Paper';
 import IconButton from 'material-ui/IconButton';
 import Divider from 'material-ui/Divider';
 import SettingsIcon from 'material-ui/svg-icons/action/settings';
+//
+import mqtt from 'mqtt';
 
 import styles from './style';
 
@@ -19,6 +22,63 @@ class App extends Component {
     this.state = {
       connectionStatus: CONNECTION_STATUS.DISCONNECTED
     };
+
+    this.mqttClient = null;
+  }
+
+  componentDidMount() {
+    this.mqttClient = mqtt.connect('mqtt://127.0.0.1:3300', {
+      reconnectPeriod: 5000
+    });
+    // this.mqttClient = mqtt.connect('mqtt://test.mosquitto.org');
+
+    this.mqttClient.on('connect', () => {
+      this.setState({
+        connectionStatus: CONNECTION_STATUS.CONNECTED
+      });
+      this.mqttClient.subscribe('test_topic');
+      this.mqttClient.publish('test_topi', 'Hello mqtt buddies');
+    });
+
+    this.mqttClient.on('reconnect', () => {
+      this.setState({
+        connectionStatus: CONNECTION_STATUS.RECONNECTING
+      });
+    });
+
+    // this.mqttClient.on('close', () => {
+    //   this.setState({
+    //     connectionStatus: CONNECTION_STATUS.DISCONNECTED
+    //   });
+    // });
+
+    this.mqttClient.on('offline', () => {
+      this.setState({
+        connectionStatus: CONNECTION_STATUS.DISCONNECTED
+      });
+    });
+
+    this.mqttClient.on('message', (topic, message) => {
+      // message is Buffer
+      console.log(message.toString());
+    });
+
+    this.mqttClient.on('error', (error) => {
+      // message is Buffer
+      console.error(error);
+    });
+
+    this.mqttClient.on('packetsend', (packet) => {
+      // message is Buffer
+      console.log('packet send:');
+      console.log(packet);
+    });
+
+    this.mqttClient.on('packetreceive', (packet) => {
+      // message is Buffer
+      console.log('packet receive');
+      console.log(packet);
+    });
   }
 
   renderConnectionStatus() {
